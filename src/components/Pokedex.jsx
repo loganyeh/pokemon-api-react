8cf5877561ff7b75338d6f834dd2f3536185fafb
loganyeh <yehlogan@gmail.com>
2025-12-03 15:26:40 -0500

    fix: handleSearch

--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -1,42 +1,19 @@
 import { useState, useRef, useEffect, useContext } from "react";
 import { MyContext } from "../context/MyContext";
 import NavBar from "./NavBar";
-import Card from "./Card";
 import Home from "../pages/Home";
 
 function Pokedex() {
-  // const {} = useContext(MyContext);
   const [inputValue, setInputValue] = useState(``);
   const [pokemon, setPokemon] = useState("");
   const [pokemonArr, setPokemonArr] = useState([]);
-
-  // useEffect(() => {
-  //     async function getData(){
-  //         // const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${inputValue}`);
-  //         const res = await fetch(`https://pokeapi.co/api/v2/pokemon/ditto`);
-  //         const data = await res.json();
-  //         setPokemon(data);
-  //     }
-
-  //     getData();
-  // }, [inputValue]);
-
-  // for(let i = 0; i < 152; i++){
-  //     console.log(i);
-  // }
+  const [pokemonCard, setPokemonCards] = useState([]);
 
   return (
     <>
       <div className="h-screen w-screen">
         <MyContext.Provider
-          value={{
-            inputValue,
-            setInputValue,
-            pokemon,
-            setPokemon,
-            pokemonArr,
-            setPokemonArr,
-          }}
+          value={{inputValue, setInputValue, pokemon, setPokemon, pokemonArr, setPokemonArr, pokemonCard, setPokemonCards}}
         >
           <NavBar />
           <Home />
