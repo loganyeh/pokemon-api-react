cee7989c3f4b3b2e929f9282d645f6f561dc1636
loganyeh <yehlogan@gmail.com>
2025-12-04 17:04:10 -0500

    fix: Favorites page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,17 @@
 import { useContext } from "react";
 import { MyContext } from "../context/MyContext";
 import { searchPokemonQuery } from "../services/api";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function NavBar() {
-  const { inputValue, setInputValue, pokemonCard, setPokemonCards, resetHome, setResetHome } = useContext(MyContext);
+  const {
+    inputValue,
+    setInputValue,
+    pokemonCard,
+    setPokemonCards,
+    resetHome,
+    setResetHome,
+  } = useContext(MyContext);
 
   const handleInputValue = (event) => {
     setInputValue(event.target.value);
@@ -22,39 +29,37 @@ function NavBar() {
     } catch (error) {
       console.error(error);
       console.log(`Pokemon not found`);
-    }
-    finally {
-
+    } finally {
     }
   };
 
   const handleConsoleClear = () => {
     console.clear();
     setInputValue(``);
-    setResetHome([prev => !prev]);
+    setResetHome([(prev) => !prev]);
   };
 
   return (
     <>
       <div className="h-1/6 w-full flex justify-around items-center text-2xl border-2 border-red-600">
-          <div onClick={handleConsoleClear} className="cursor-pointer">
-            <Link to="/">Home</Link>
-          </div>
-          <div>
-            <input
-              value={inputValue}
-              onChange={handleInputValue}
-              type="text"
-              className="h-10 w-50 border-2 border-red-600"
-            />
-            <button
-              onClick={handleSearch}
-              className="h-10 w-20 border-2 border-red-600 hover:bg-gray-200 active:bg-gray-100 cursor-pointer"
-            >
-              Search
-            </button>
-          </div>
-          <div>Favorites</div>
+        <div onClick={handleConsoleClear} className="cursor-pointer">
+          <Link to="/">Home</Link>
+        </div>
+        <div>
+          <input
+            value={inputValue}
+            onChange={handleInputValue}
+            type="text"
+            className="h-10 w-50 border-2 border-red-600"
+          />
+          <button
+            onClick={handleSearch}
+            className="h-10 w-20 border-2 border-red-600 hover:bg-gray-200 active:bg-gray-100 cursor-pointer"
+          >
+            Search
+          </button>
+        </div>
+        <div>Favorites</div>
       </div>
     </>
   );
--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -2,7 +2,7 @@ import { useState, useRef, useEffect, useContext } from "react";
 import { MyContext } from "../context/MyContext";
 import NavBar from "./NavBar";
 import Home from "../pages/Home";
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
 function Pokedex() {
   const [inputValue, setInputValue] = useState(``);
@@ -15,12 +15,25 @@ function Pokedex() {
   return (
     <>
       <div className="h-screen w-screen">
-        <MyContext.Provider value={{inputValue, setInputValue, pokemon, setPokemon, pokemonArr, setPokemonArr, pokemonCard, setPokemonCards, resetHome, setResetHome, pokemonSpriteArr, setPokemonSpriteArr}}>
+        <MyContext.Provider
+          value={{
+            inputValue,
+            setInputValue,
+            pokemon,
+            setPokemon,
+            pokemonArr,
+            setPokemonArr,
+            pokemonCard,
+            setPokemonCards,
+            resetHome,
+            setResetHome,
+            pokemonSpriteArr,
+            setPokemonSpriteArr,
+          }}
+        >
+          <NavBar/>
           <Routes>
-            <Route path="/" element={<NavBar />}/>
-          </Routes>
-          <Routes>
-            <Route path="/" element={<Home />}/>
+            <Route path="/" element={<Home />} />
           </Routes>
         </MyContext.Provider>
       </div>
