ffea7090f504b69e51070366f2fce7155b35fdf4
loganyeh <yehlogan@gmail.com>
2025-12-03 19:41:57 -0500

    feat: filtering search

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@
 // https://pokeapi.co/api/v2/pokemon/ditto
 
 export const getPokemon = async () => {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=5&offset=0`);
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=151&offset=0`);
     const data = await response.json();
     return data.results;
 }
