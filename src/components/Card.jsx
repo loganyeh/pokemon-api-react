06eac8203702bccd1d875ba3df2fb643e7a6b45d
loganyeh <yehlogan@gmail.com>
2025-12-04 19:28:09 -0500

    feat: isFavorite hover/active

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,20 +2,20 @@ import { useContext, useState } from "react";
 import { MyContext } from "../context/MyContext";
 
 function Card({ pokemon, counter, sprite }) {
-  const [isFavorite, setIsFavorite] = useState(false);
+  const [isFavorite, setIsFavorite, favArray, setFavArray] = useState(false);
 
   // click click and state variable boolean for click and set it to the number id
   // then map it to a state array and then add to favorites page
   function handleFavorite() {
     setIsFavorite(prev => !prev);
-    console.log(isFavorite);
+    
   }
 
   return (
     <>
       <div className="h-95 w-75 grid grid-rows-12 grid-cols-12 mb-10 border-2 border-yellow-600 rounded-2xl">
         <div onClick={handleFavorite} className={`border-2 border-black row-start-1 row-end-3 col-start-1 col-end-4 hover:bg-gray-200 active:bg-gray-300
-        ${isFavorite ? `bg-red-600` : ``}
+        ${isFavorite ? `bg-red-600 hover:bg-red-500 active:bg-red-700` : ``}
         `}>
           #{(counter)} 
         </div>
--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,13 +1,16 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { MyContext } from "../context/MyContext";
+import Card from "../components/Card";
 
 function Favorites(){
     const {favArray, setArray} = useContext(MyContext);
 
-    return(
+    return (
         <>
             <div className="h-full w-full flex justify-center border-2 border-blue-600">
                 <h1>This is the favorites</h1>
+
+                {/* {favArray.map((fav, index) => <Card key={index} pokemon={} counter={index + 1}/>)} */}
 
             </div>
         </>
